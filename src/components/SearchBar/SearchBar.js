2609d6360bab97bbd000a18c75d7e3a91785ad35
ExciteTaco <ericklima34@gmail.com>
2020-04-24 22:26:00 -0500

    changes to the searchbar component

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -2,16 +2,30 @@ import React from "react";
 import "./SearchBar.css";
 // import { render } from "@testing-library/react";
 
-const sortByOptions = {
-  "Best Match": "best_match",
-  "Highest Rated": "rating",
-  "Most Reviewed": "review_count",
-};
-
 class SearchBar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      term: "",
+      location: "",
+      sortBy: "best_match",
+    };
+    this.sortByOptions = {
+      "Best Match": "best_match",
+      "Highest Rated": "rating",
+      "Most Reviewed": "review_count",
+    };
+  }
+  getSortByClass() {
+    if (this.state.sortBy === this.sortByOptions.sortBy) return "active";
+    else return "";
+  }
+  handleSortByChange(sortByOption) {
+    this.setState({ sortBy: sortByOption });
+  }
   renderSortByOptions() {
-    return Object.keys(sortByOptions).map((sortByOption) => {
-      let sortByOptionValue = sortByOptions[sortByOption];
+    return Object.keys(this.sortByOptions).map((sortByOption) => {
+      let sortByOptionValue = this.sortByOptions[sortByOption];
       return <li key="sortByOptionValue">{sortByOption}</li>;
     });
   }
