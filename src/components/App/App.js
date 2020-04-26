9be06a4d93107c78d75a8e2360c1852d0bb9a6bd
ExciteTaco <ericklima34@gmail.com>
2020-04-25 22:55:20 -0500

    Add comments to teh App.js component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,7 +3,7 @@ import React from "react";
 import "./App.css";
 import BusinessList from "../BusinessList/BusinessList";
 import SearchBar from "../SearchBar/SearchBar";
-
+// Temporary object that sets an example on how the data that will returned is formatted
 const business = {
   imageSrc:
     "https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg",
@@ -16,7 +16,7 @@ const business = {
   rating: 4.5,
   reviewCount: 90,
 };
-
+// Creating an arrays made of of mulitple object created above
 const businesses = [business, business, business, business, business, business];
 
 function App() {
@@ -24,6 +24,7 @@ function App() {
     <div className="App">
       <h1>ravenous</h1>
       <SearchBar />
+      {/* Passing down props to the BusinessList component */}
       <BusinessList businesses={businesses} />
     </div>
   );
