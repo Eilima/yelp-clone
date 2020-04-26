9862977732a6d8eab2e550122a7dc3ac5b97be07
ExciteTaco <ericklima34@gmail.com>
2020-04-25 22:53:07 -0500

    Add comments to the BusinessList.js component

--- src/components/BusinessList/BusinessList.js
+++ src/components/BusinessList/BusinessList.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./BusinessList.css";
 import Business from "../Business/Business";
-
+// Is passed down the 'Businesses' array which contains objects
 class BusinessList extends React.Component {
+  // Will render the a instance of the 'Business' component for each element of the array
   render() {
     return (
       <div className="BusinessList">
+        {/* Maps through each element of the passed down array and returns Business component with each element of the arrays */}
         {this.props.businesses.map((business) => {
           return <Business business={business} />;
         })}
