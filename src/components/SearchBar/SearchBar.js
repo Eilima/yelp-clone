c141a0001c956fe7f8859ad6ecb856a2044a54bd
ExciteTaco <ericklima34@gmail.com>
2020-05-19 19:40:03 -0500

    Add handleSearch method

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -52,6 +52,14 @@ class SearchBar extends React.Component {
       );
     });
   }
+  handleSearch = (event) => {
+    this.props.searchYelp(
+      this.state.term,
+      this.state.location,
+      this.state.sortBy
+    );
+    event.preventDefault();
+  };
 
   render() {
     return (
