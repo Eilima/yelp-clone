e76d28aca21c570597f974c4258fef18d55c52fe
Eilima <ericklima34@gmail.com>
2020-08-06 15:40:52 -0500

    Delete constructor

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,15 +1,18 @@
 import React from "react";
 import "./App.css";
+
 import BusinessList from "../BusinessList/BusinessList";
 import SearchBar from "../SearchBar/SearchBar";
+
 import Yelp from "../../util/Yelp";
 
 class App extends React.Component {
   state = {
     businesses: [],
   };
+
   searchYelp = (term, location, sortBy) => {
-    Yelp.search(term, location, sortBy).then((businesses) => {
+    Yelp.searchYelp(term, location, sortBy).then((businesses) => {
       this.setState({
         businesses: businesses,
       });
@@ -19,7 +22,7 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <h1>ravenous</h1>
+        <h1>Yelp Clone</h1>
         <SearchBar searchYelp={this.searchYelp} />
         <BusinessList businesses={this.state.businesses} />
       </div>
