7293e5409727504f32cc2cd7992a059a0c3b32d4
Eilima <ericklima34@gmail.com>
2020-08-06 15:25:18 -0500

    update to be an async funtion

--- src/util/Yelp.js
+++ src/util/Yelp.js
@@ -1,24 +1,33 @@
-const apiKey = "";
-
+const apiKey =
+  "7VoKtR0Xv74YUgYbPe7nxgm_bI9XAHVzv4dNKtb1l9jJlMClox8DGfhYys5NuYosox87UWytNI8-zUSXfyiQM-CvmUzzvETFg-s5l7xC4AxIToEOAygmMvlYPjkeX3Yx"; // Insert API key here.
 const Yelp = {
-    search = (term, location, sortBy) => {
-        return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`, header = {Authorization: `Bearer ${apiKey}`})
-        .then(response => {return response.json()})
-        .then(jsonResponse => {if(jsonResponse.businesses){
-            return jsonResponse.businesses.map(business => {return {id: business.id,
-            imageSrc: business.image_url,
-            name: business.name,
-            address: business.address,
-            city: business.city,
-            state: business.state,
-            zipCode: business.zipCode,
-            category: business.category,
-            rating: business.rating,
-            reviewCount: business.reviewCount,
-        }})
-        }})
+  async search(term, location, sortBy) {
+    const response = await fetch(
+      `https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`,
+      {
+        headers: {
+          Authorization: `Bearer ${apiKey}`,
+        },
+      }
+    );
+    const data = await response.json();
+    console.log(data);
+    if (data.businesses) {
+      return data.businesses.map((business) => ({
+        id: business.id,
+        imageSrc: business.image_url,
+        name: business.name,
+        address: business.location.address1,
+        city: business.location.city,
+        state: business.location.state,
+        zipCode: business.location.zip_code,
+        category: business.categories[0].title,
+        rating: business.rating,
+        reviewCount: business.review_count,
+        url: business.url,
+      }));
     }
-}
+  },
+};
 
-
-export default Yelp; 
+export default Yelp;
