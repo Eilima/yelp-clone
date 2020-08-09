a35f22932c5d91dfc1a81ede40a40020c685442d
Eilima <ericklima34@gmail.com>
2020-08-09 11:34:42 -0500

    Perform google search on Google Maps

--- src/components/Business/Business.js
+++ src/components/Business/Business.js
@@ -2,6 +2,13 @@ import React from "react";
 import "./Business.css";
 
 class Business extends React.Component {
+  // Return a Google Maps link to the location of the business
+  googleMapsLink = () => {
+    return `https://www.google.com/maps/place/${this.props.business.address
+      .split(" ")
+      .join("+")}+${this.props.business.city.split(" ").join("+")}`;
+  };
+
   render() {
     return (
       <div className="Business">
@@ -13,9 +20,11 @@ class Business extends React.Component {
         <h2>{this.props.business.name}</h2>
         <div className="Business-information">
           <div className="Business-address">
-            <p>{this.props.business.address}</p>
-            <p>{this.props.business.city}</p>
-            <p>{`${this.props.business.state} ${this.props.business.zipCode}`}</p>
+            <a href={this.googleMapsLink()}>
+              <p>{this.props.business.address}</p>
+              <p>{this.props.business.city}</p>
+              <p>{`${this.props.business.state} ${this.props.business.zipCode}`}</p>
+            </a>
           </div>
           <div className="Business-reviews">
             <h3>{this.props.business.category.toUpperCase()}</h3>
